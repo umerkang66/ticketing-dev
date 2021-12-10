b099422729371cade4755f0c72709dfba6dbe629
Umer Kang <umerkang66@gmail.com>
2021-12-10 18:29:29 +0500

    add skaffold yaml file

---
+++ auth/src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import { json } from 'body-parser';
+
+const app = express();
+app.use(json());
+
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('Hi there');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}`);
+});
