465b37195f596cd591456920df52c8f9b0989873
Umer Kang <umerkang66@gmail.com>
2021-12-14 22:51:25 +0500

    complete the normalization section of microservices course

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -9,17 +9,13 @@ router.post(
   '/api/users/signup',
   [
     // Body is going to check the body of the incoming request
-    body('email')
-      .isEmail()
-      .withMessage(
-        'Email that is provided must be valid according the email standards'
-      ),
+    body('email').isEmail().withMessage('Email that is provided must be valid'),
     body('password')
       .trim()
       .isLength({ min: 4, max: 20 })
       .withMessage('Password must be between 4 and 20 characters '),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
