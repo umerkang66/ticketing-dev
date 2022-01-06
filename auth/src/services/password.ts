a88640c9fc4b56c876aa73d0cbc1e1528eb15af1
Umer Kang <umerkang66@gmail.com>
2022-01-06 10:30:32 +0500

    add comment in password.ts file

--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -6,7 +6,7 @@ const scryptAsync = promisify(scrypt);
 export class Password {
   // Static methods are the methods that can be access without creating an instance of class (it can be accessed from the class itself)
   static async toHash(password: string) {
-    // Async process are handled in event loop but heavy processes like hashing (crypto module) are sent off to the libuv library
+    // Async process are handled in event loop but heavy processes like hashing (crypto module) are sent off to the Thread Pool in the libuv library
     // Salt is the part of hashing process
     const salt = randomBytes(8).toString('hex');
     // Hashing || This returns a buffer (a buffer is like an array with raw data in it)
