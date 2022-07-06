5ef3ab5ab2afbfdf8b2850a86d5c74355d58b4aa
umerkang66 <umerkang66@gmail.com>
2022-07-06 17:14:45 +0500

    start building the payments service

---
+++ payments/src/app.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+
+// Errors
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from '@ticketing-umer/common';
+
+const app = express();
+// traffic is being proxy to our server through ingress nginx, and trust the proxies
+app.set('trust proxy', true);
+app.use(express.json());
+
+// jest automatically set the env to 'test'
+const isSecure = process.env.NODE_ENV !== 'test';
+// "signed": don't encrypt this
+// "secure": only set cookies on https
+app.use(cookieSession({ signed: false, secure: isSecure }));
+
+app.use(currentUser);
+
+app.all('*', (req, res) => {
+  const errMsg = `'${req.method}: ${req.originalUrl}' does not found on this server`;
+  throw new NotFoundError(errMsg);
+});
+
+// Error Handler middleware
+app.use(errorHandler);
+
+export { app };
